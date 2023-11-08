1bbd7699c6debcfab0fe71f096b1cc3779cad4e3
vik <vik1988kms@gmail.com>
2023-11-08 21:04:41 +0300

    edited insertSeorting

--- chapter_06/01_insert_sorting.py
+++ chapter_06/01_insert_sorting.py
@@ -2,9 +2,23 @@
 O(N^2) - говорит об полиномиальном росте.
 """
 
-def insertSeorting(array):
-    pass
+def insertSorting(array):
+    for i in range(1, len(array)):
+        value = array[i]
+        j = i - 1
+
+        while j >= 0:
+            if array[i-1] > value:
+                array[i] = array[i-1]
+                index = i-1
+                j -= 1
+            else:
+                break
+        array[index] = value
+
+
+
 
 
 if __name__ == '__main__':
-    print(insertSeorting([7, 2, 5, 9, 8, 1]))
+    print(insertSorting([7, 2, 5, 9, 8, 1]))
